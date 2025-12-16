314c4503b03a0725147db09cde6a4dd122a6b794
molla-12 <mollaman19@gmail.com>
2025-12-16 15:02:25 +0300

    check user is login

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
-import LoginPage from "./login/page";
+'use client'
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+import LoginPage from './login/page'
 
 export default function Home() {
-  return <LoginPage />;
+  const router = useRouter()
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      router.replace('/chat')
+    }
+  }, [router])
+
+  return <LoginPage />
 }
